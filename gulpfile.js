36a3cb3333532200305dd8c9b4a7cd9ef077a8ec
severjason <ssever2010@gmail.com>
2015-11-25 21:08:50 +0200

    Add gulp connect

--- gulpfile.js
+++ gulpfile.js
@@ -6,57 +6,68 @@ var gulp = require('gulp'),
     cleanDest = require('gulp-clean-dest'),
     concat = require('gulp-concat'),
     browserify = require('gulp-browserify'),
-    rubySass = require('gulp-ruby-sass'),
+    connect = require('gulp-connect'),
     compass = require('gulp-compass'),
     coffee = require('gulp-coffee');
 
 var sources = {
-    coffee:['components/coffee/tagline.coffee'],
-    js : [
+    coffee: ['components/coffee/tagline.coffee'],
+    js: [
         'components/scripts/rclick.js',
         'components/scripts/pixgrid.js',
         'components/scripts/tagline.js',
         'components/scripts/template.js'
     ],
-    sass:['components/sass/style.scss'],
-    sassPartials:['components/sass/*.scss']
+    sass: ['components/sass/style.scss'],
+    sassPartials: ['components/sass/*.scss'],
+    development: ['builds/development']
 };
 
-gulp.task('coffee', function() {
+gulp.task('coffee', function () {
     gulp.src(sources.coffee)
         .pipe(coffee({bare: true})
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
 });
-gulp.task('js', function() {
+gulp.task('js', function () {
     gulp.src(sources.js)
         .pipe(concat('script.js'))
         .pipe(browserify()
-            .on('error',gutil.log))
+            .on('error', gutil.log))
         .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload())
 });
 
-gulp.task('compass', function() {
+gulp.task('compass', function () {
     gulp.src(sources.sass)
         .pipe(compass({
-            sass:'components/sass',
-            image:'builds/development/images',
-            style:'expanded',
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded',
             comments: true
         }))
-        .on('error', function(error) {
-        // Would like to catch the error here
-        console.log(error);
-        this.emit('end');
-    })
+        .on('error', function (error) {
+            // Would like to catch the error here
+            console.log(error);
+            this.emit('end');
+        })
         .pipe(gulp.dest('builds/development/css'))
         .pipe(cleanDest('css'))
+        .pipe(connect.reload())
 });
 
-gulp.task('watch', function(){
-    gulp.watch(sources.coffee,['coffee']);
-    gulp.watch(sources.js,['js']);
-    gulp.watch(sources.sassPartials,['compass']);
+gulp.task('watch', function () {
+    gulp.watch(sources.coffee, ['coffee']);
+    gulp.watch(sources.js, ['js']);
+    gulp.watch(sources.sassPartials, ['compass']);
+});
+gulp.task('connect', function () {
+    connect.server({
+        port: 8888,
+        root: sources.development,
+        livereload: true
+    })
+
 });
 
-gulp.task('default',['coffee','js','compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
